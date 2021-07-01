8f3496ac7c810df9176a527554b76ecdc68ae664
raiden <jhon.jaramilloe@udea.edu.co>
2021-06-30 20:51:21 -0500

    fin_proyect

--- javascript/your_age_in_days/static/js/script.js
+++ javascript/your_age_in_days/static/js/script.js
@@ -1,1 +1,16 @@
-alert('Hello word')
+// challenge 1: Your Age in Days
+
+function ageInDays() {
+    let birthYear = prompt('what year were you born... Good friend');
+    let ageInDays = (2020 - birthYear) * 365;
+    let h1 = document.createElement('h1');
+    let textAnswer = document.createTextNode('You are ' + ageInDays + ' days old');
+    h1.setAttribute('id', 'ageInDays');
+    h1.appendChild(textAnswer);
+    document.getElementById('flex-box-result').appendChild(h1);
+
+}
+
+function reset() {
+    document.getElementById('ageInDays').remove();
+}
