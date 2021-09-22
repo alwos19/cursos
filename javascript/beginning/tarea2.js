d39e60fcd059bf96756dab817eb8256daad55b2e
alwos19 <alwos19>
2021-09-22 15:20:35 -0500

    asistencia

--- javascript/beginning/tarea2.js
+++ javascript/beginning/tarea2.js
@@ -1,13 +1,13 @@
-let cantidad = prompt("Cuantos alumnos son");
+let cantidad = prompt("Cuantos alumnos son?");
 let alumnosTotales= [];
 
 for (i=0; i < cantidad; i++) {
     alumnosTotales[i] = [prompt("Nombre del alumno " + (i+1)), 0];
 }
 
-const tomarAsistencia = (nombre,p)=>{
-    let prsencia = prompt(nombre);
-    if (precencia == "p" || presencia == "P") {
+const tomarAsistencia = (nombre,p) => {
+    let presencia = prompt(nombre);
+    if (presencia == "p" || presencia == "P") {
         alumnosTotales[p][1]++;
     }
 }
@@ -16,4 +16,16 @@ for (i = 0; i < 30; i++) {
     for (alumno in alumnosTotales) {
         tomarAsistencia(alumnosTotales[alumno][0],alumno);
     }
+}
+
+for (alumno in alumnosTotales) {
+    let resultado = `${alumnosTotales[alumno][0]}:<br>
+    ________Presentes: ${alumnosTotales[alumno][1]}:<br>
+    ________Ausencias: ${30 - parseInt(alumnosTotales[alumno][1])}`;
+    if (30 -alumnosTotales[alumno][1] > 18) {
+        resultado+= "<b style='color:red'>Reprobado por inasistencia</b><br><br>";
+    }else{
+        resultado+= "<br><br>"
+    }
+    document.write(resultado);
 }
