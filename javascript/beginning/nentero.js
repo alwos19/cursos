c96482faef661b7b0864157efb86b943133eaa1a
alwos19 <jhon.jaramilloe@udea.edu.co>
2022-02-11 22:25:19 -0500

    la suma de n

---
+++ javascript/beginning/nentero.js
@@ -0,0 +1,10 @@
+let entero = prompt("ingrese número entero");
+
+
+
+//let suma = (parseInt(entero)*parseInt(entero+1));
+
+let suma = parseFloat(entero)*((parseFloat(entero)+parseFloat(1)))/parseFloat(2);
+
+alert(`La suma de los números enteros son ${suma}`);
+
